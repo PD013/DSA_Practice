be0e4603b79a45dd6df96d0234c4cb5b44d3e0f8
Preet Desai <114251906+PD013@users.noreply.github.com>
2024-01-16 21:59:19 +0530

    Delete LC_DSA_Sums/217-contains-duplicate directory

--- LC_DSA_Sums/217-contains-duplicate/contains-duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-  def containsDuplicate(self, nums):
-   return len(nums) != len(set(nums))
-
-nums = [1,2,3,1]
-result = Solution().containsDuplicate(nums)
-print(result)
-
-        
