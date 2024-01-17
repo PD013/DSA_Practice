7a30bbf4d4e06a113190105cb8d5dbfa89ec15e8
Preet Desai <114251906+PD013@users.noreply.github.com>
2024-01-17 09:20:56 +0530

    Time: 919 ms (82.76%) | Memory: 0B (100.00%) - LeetSync

--- LC_DSA_Sums/1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ LC_DSA_Sums/1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,15 +1,10 @@
 # Write your MySQL query statement below
--- SELECT employee_id, department_id
--- FROM Employee
--- WHERE primary_flag = 'Y' OR employee_id in (
---     SELECT employee_id # This finds employee who has only one department
---     FROM Employee # And employee with only one dep will have flag = N 
---     GROUP BY employee_id 
---     HAVING COUNT(*) = 1 
--- )
--- GROUP BY employee_id
-
 SELECT employee_id, department_id
 FROM Employee
-WHERE primary_flag = 'Y' OR employee_id in (SELECT employee_id FROM Employee  GROUP BY employee_id HAVING COUNT(*) = 1)
+WHERE primary_flag = 'Y' OR employee_id in (
+    SELECT employee_id # This finds employee who has only one department
+    FROM Employee # And employee with only one dep will have flag = N 
+    GROUP BY employee_id 
+    HAVING COUNT(*) = 1 
+)
 GROUP BY employee_id
