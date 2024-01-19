e359b5c06a6b096125d658d2b5b54e115ce02c62
Preet Desai <114251906+PD013@users.noreply.github.com>
2024-01-19 12:00:58 +0530

    Time: 754 ms (28.24%) | Memory: 0B (100.00%) - LeetSync

--- LC_DSA_Sums/2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ LC_DSA_Sums/2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,9 +1,3 @@
-# Write your MySQL query statement below
--- SELECT employee_id 
--- FROM Employees
--- WHERE salary < 30000
--- AND employee_id in (SELECT employee_id Employees WHERE manager_id is NOT NULL )
-
 SELECT employee_id FROM Employees WHERE salary < 30000 
 AND 
 manager_id not in (SELECT DISTINCT employee_id from Employees)
