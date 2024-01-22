4a0baa07beed8d02203c5b535e0bc158112d0b40
Preet Desai <114251906+PD013@users.noreply.github.com>
2024-01-23 02:43:34 +0530

    Time: 994 ms (80.51%) | Memory: 0B (100.00%) - LeetSync

---
+++ LC_DSA_Sums/1827-invalid-tweets/invalid-tweets.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+# While 'LENGTH' function measures the length in bytes, 'CHAR_LENGTH' function measures the length of a string in characters
+
+# This is the correct solution 
+SELECT tweet_id FROM Tweets
+WHERE CHAR_LENGTH(content) > 15
