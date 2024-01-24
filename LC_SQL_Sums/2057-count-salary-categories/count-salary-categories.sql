411079b19cc31d8db821066926c02f31345d88e6
Preet Desai <114251906+PD013@users.noreply.github.com>
2024-01-24 15:29:33 +0530

    Time: 2717 ms (50.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ LC_SQL_Sums/2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+WITH CTE as 
+(SELECT CASE
+    WHEN income > 50000 THEN 'High Salary'
+    WHEN income < 20000 THEN 'Low Salary'
+    WHEN income>=20000 AND income<=50000 THEN 'Average Salary' 
+    END AS category,  account_id ,income
+FROM Accounts )
+,
+cte2 as(
+select 'Low Salary' level
+union
+select 'Average Salary'
+union
+select 'High Salary'
+)
+
+
+select level as category, ifnull(count(income),0) as accounts_count 
+from CTE 
+right join cte2 on cte.category = cte2.level
+group by cte2.level;
