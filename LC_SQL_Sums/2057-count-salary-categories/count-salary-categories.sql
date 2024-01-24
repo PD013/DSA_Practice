96fc43f4e8883350fa48875fec4896c142cd0cb2
Preet Desai <114251906+PD013@users.noreply.github.com>
2024-01-24 15:31:21 +0530

    Time: 2846 ms (41.96%) | Memory: 0B (100.00%) - LeetSync

--- LC_SQL_Sums/2057-count-salary-categories/count-salary-categories.sql
+++ LC_SQL_Sums/2057-count-salary-categories/count-salary-categories.sql
@@ -1,22 +1,31 @@
 # Write your MySQL query statement below
-WITH CTE as 
-(SELECT CASE
-    WHEN income > 50000 THEN 'High Salary'
-    WHEN income < 20000 THEN 'Low Salary'
-    WHEN income>=20000 AND income<=50000 THEN 'Average Salary' 
-    END AS category,  account_id ,income
-FROM Accounts )
-,
-cte2 as(
-select 'Low Salary' level
-union
-select 'Average Salary'
-union
-select 'High Salary'
-)
 
 
-select level as category, ifnull(count(income),0) as accounts_count 
-from CTE 
-right join cte2 on cte.category = cte2.level
-group by cte2.level;
+Select "Low Salary" as category, count(income) as "accounts_count" from Accounts where income<20000
+union all
+Select "Average Salary" as category, count(income) as "accounts_count" from Accounts where income >=20000 and income<=50000
+union all
+Select "High Salary" as category, count(income) as "accounts_count" from Accounts where income > 50000
+
+
+-- WITH CTE as 
+-- (SELECT CASE
+--     WHEN income > 50000 THEN 'High Salary'
+--     WHEN income < 20000 THEN 'Low Salary'
+--     WHEN income>=20000 AND income<=50000 THEN 'Average Salary' 
+--     END AS category,  account_id ,income
+-- FROM Accounts )
+-- ,
+-- cte2 as(
+-- select 'Low Salary' level
+-- union
+-- select 'Average Salary'
+-- union
+-- select 'High Salary'
+-- )
+
+
+-- select level as category, ifnull(count(income),0) as accounts_count 
+-- from CTE 
+-- right join cte2 on cte.category = cte2.level
+-- group by cte2.level;
