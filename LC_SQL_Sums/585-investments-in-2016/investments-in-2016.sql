78f2ec2d041b6528a8104682d350fd8bb7950f11
Preet Desai <114251906+PD013@users.noreply.github.com>
2024-01-25 16:12:46 +0530

    Time: 951 ms (50.76%) | Memory: 0B (100.00%) - LeetSync

--- LC_SQL_Sums/585-investments-in-2016/investments-in-2016.sql
+++ LC_SQL_Sums/585-investments-in-2016/investments-in-2016.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+# Write your MySQL query statement below
 
 SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
 FROM Insurance
@@ -6,7 +7,7 @@ WHERE (lat, lon) IN (
     SELECT lat, lon
     FROM Insurance
     GROUP BY lat, lon
-    HAVING COUNT(pid) = 1
+    HAVING COUNT(pid) = 1 
 )
 AND tiv_2015 IN
 (SELECT tiv_2015
@@ -14,3 +15,12 @@ AND tiv_2015 IN
     GROUP BY tiv_2015
     HAVING COUNT(pid) > 1
 );
+
+-- SELECT ROUND(SUM(TIV_2016),2) AS TIV_2016
+-- FROM
+-- (SELECT *,
+-- COUNT(*) OVER(PARTITION BY TIV_2015) AS CNT1,
+-- COUNT(*) OVER(PARTITION BY LAT, LON) AS CNT2
+-- FROM INSURANCE
+-- ) AS TBL
+-- WHERE CNT1 > =2 AND CNT2 =1
