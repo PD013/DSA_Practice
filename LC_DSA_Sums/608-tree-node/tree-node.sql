5a16e2418f484fe6686194f49c478da57b5f1517
Preet Desai <114251906+PD013@users.noreply.github.com>
2024-09-11 02:12:54 +0530

    Time: 518 ms (25.38%) | Memory: 0B (100.00%) - LeetSync

--- LC_DSA_Sums/608-tree-node/tree-node.sql
+++ LC_DSA_Sums/608-tree-node/tree-node.sql
@@ -1,22 +1,37 @@
 
 
-SELECT id,type FROM 
-(SELECT
-    Tree.id,
-    COUNT(sub.id) AS total,
-    CASE
-        WHEN EXISTS (SELECT 1 FROM Tree AS t WHERE t.id = Tree.id AND t.p_id IS NULL) THEN 'Root'
-        WHEN COUNT(sub.id) > 0 THEN 'Inner'
-        ELSE 'Leaf'
-    END AS type
-FROM
-    Tree
-LEFT JOIN
-    Tree AS sub
-ON
-    Tree.id = sub.p_id
-GROUP BY
-    Tree.id
-ORDER BY
-    Tree.id) temp;
+-- SELECT id,type FROM 
+-- (SELECT
+--     Tree.id,
+--     COUNT(sub.id) AS total,
+--     CASE
+--         WHEN EXISTS (SELECT 1 FROM Tree AS t WHERE t.id = Tree.id AND t.p_id IS NULL) THEN 'Root'
+--         WHEN COUNT(sub.id) > 0 THEN 'Inner'
+--         ELSE 'Leaf'
+--     END AS type
+-- FROM
+--     Tree
+-- LEFT JOIN
+--     Tree AS sub
+-- ON
+--     Tree.id = sub.p_id
+-- GROUP BY
+--     Tree.id
+-- ORDER BY
+--     Tree.id) temp;
 
+
+--  The Below is the bestttt solution
+-- SELECT id,
+
+--     CASE 
+--         WHEN p_id IS NULL THEN 'Root'
+--         WHEN id IN (SELECT p_id FROM Tree)THEN 'Inner'
+--         ELSE 'Leaf'
+--         END AS type
+--  FROM Tree
+
+-- ====
+SELECT id, IF(p_id IS NULL, "Root",
+IF(id IN (SELECT p_id FROM Tree), "Inner", "Leaf")) AS type
+FROM Tree
