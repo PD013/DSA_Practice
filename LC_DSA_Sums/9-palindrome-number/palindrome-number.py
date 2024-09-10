f1cec89060bc65a7992241860cabd96d479b26f3
Preet Desai <114251906+PD013@users.noreply.github.com>
2024-09-10 17:10:53 +0530

    Time: 51 ms (21.94%) | Memory: 11.6 MB (32.47%) - LeetSync

--- LC_DSA_Sums/9-palindrome-number/palindrome-number.py
+++ LC_DSA_Sums/9-palindrome-number/palindrome-number.py
@@ -1,11 +1,12 @@
 class Solution(object):
     def isPalindrome(self, x):
-        if x < 0:
-            return False
-        if x == 0:
-            return True
-        reversed_str = int(str(x)[::-1])
-        if x == reversed_str:
-            return True
-        else :
-            return False
+        return str(x) == str(x)[::-1]
+        # if x < 0:
+        #     return False
+        # if x == 0:
+        #     return True
+        # reversed_str = int(str(x)[::-1])
+        # if x == reversed_str:
+        #     return True
+        # else :
+        #     return False
