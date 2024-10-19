145832e012832fe149336beb70fc1d9a5565f308
Preet Desai <114251906+PD013@users.noreply.github.com>
2024-10-19 23:55:26 +0530

    Time: 303 ms (18.62%) | Memory: 0B (100.00%) - LeetSync

--- LC_SQL_Sums/610-triangle-judgement/triangle-judgement.sql
+++ LC_SQL_Sums/610-triangle-judgement/triangle-judgement.sql
@@ -1,5 +1,13 @@
 # Write your MySQL query statement below
-SELECT x, y, z,
-       CASE WHEN x+y > z AND x+z > y AND y+z > x THEN 'Yes' ELSE 'No' END AS triangle
-FROM Triangle;
+SELECT x,y,z,
+CASE # At any time 2 Sides of Triangle shall be greater than the 3rd
+    WHEN x + y <= z THEN 'No'
+    WHEN y + z <= x THEN 'No'
+    WHEN z + x <= y THEN 'No'
+    ELSE 'Yes'
+END AS triangle 
+FROM Triangle 
 
+# Found this technique using IF(conditions,if proven codition,else condition)
+# Write your MySQL query statement below
+-- select *, if(x+y>z and y+z>x and z+x>y, "Yes", "No") as triangle from Triangle
