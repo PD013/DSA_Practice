fef28e9a2957d212d0dbb4d491b3f8fa3d1dd162
Preet Desai <114251906+PD013@users.noreply.github.com>
2025-05-21 00:33:03 +0530

    Time: 290 ms (39.66%) | Memory: 0B (100.00%) - LeetSync

--- LC_SQL_Sums/595-big-countries/big-countries.sql
+++ LC_SQL_Sums/595-big-countries/big-countries.sql
@@ -1,9 +1,18 @@
-# Write your MySQL query statement below
-select name
-    , population
-    , area
-from World 
-where 
-    area >= 3000000
-    OR 
-    population >= 25000000
+-- # Write your MySQL query statement below
+-- select name
+--     , population
+--     , area
+-- from World 
+-- where 
+--     area >= 3000000
+--     OR 
+--     population >= 25000000
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000
+
+UNION 
+
+SELECT name, population, area
+FROM World
+WHERE population >= 25000000
